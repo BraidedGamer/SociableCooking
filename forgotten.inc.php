63662b49c7f1f7e871e5a5a30d4d3870a0409ab9
MasterProgram <webmaster@sociablecooking.com>
2014-03-01 15:28:38 -0600

    Patching the forgotten inc file I have finished the look of the forgotten.inc php file and in the process had to add modify the fieldStyle css stylesheet. Now the forgotten.inc php file has a form and next we'll work on the reset file and a new mysql database table.

--- forgotten.inc.php
+++ forgotten.inc.php
@@ -5,11 +5,21 @@
  *you are.
 */
 
-echo "This is some place holder text...";
-
 echo "<h1>Sorry For The Inconvience</h1>\n";
 echo "<p>So you've forgotten your password, well luckily we've thought ahead.";
 echo " Please fill out the following form and you should recieve an email ";
 echo "shortly. Said email will contain a link to the reset password form.</p>";
+
+echo "<form action=\"index.php\" method=\"post\" target=\"_self\">\n";
+echo "<table width=\"200px\" border=\"0\" cellspacing=\"2.5px\" cellpadding=\"0\" align=\"center\">\n";
+echo "<tr><td align=\"right\"><font color=\"#000\">\n";
+echo "<b>E-Mail</b></font></td>\n";
+echo "<td align=\"left\">\n";
+echo "<input type=\"text\" name=\"email-retrieval\" id=\"emailRetrieval\">\n";
+echo "</td></tr>\n";
+echo "<tr><td colspan=\"2\" align=\"right\">\n";
+echo "<input type=\"submit\" name=\"forgotten\" id=\"emailRetrievalbtn\" value=\"Forgotten Password\"></td></tr>\n";
+echo "<tr><td><input type=\"hidden\" value=\"reset-password\" name=\"card\"></td></tr>\n";
+echo "</table></form>\n";
 
 ?>
