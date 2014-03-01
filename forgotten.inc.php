2934a2d436545c6e14b51c62b5c78df08aa14f46
MasterProgram <webmaster@sociablecooking.com>
2014-03-01 01:44:30 -0600

    New File forgotten.inc.php The forgotten.inc.php has been created and has a few basic text lines. Next I'll add the form followed by the mysql table to the database. When I create the reset password script I'll most likely be updating to a better and more secure encryption system.

---
+++ forgotten.inc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *This is the forgotten password screen with a form to submit your email to a 
+ *mysql database along with a uniqueid identifier to ensure you are who you say
+ *you are.
+*/
+
+echo "This is some place holder text...";
+
+echo "<h1>Sorry For The Inconvience</h1>\n";
+echo "<p>So you've forgotten your password, well luckily we've thought ahead.";
+echo " Please fill out the following form and you should recieve an email ";
+echo "shortly. Said email will contain a link to the reset password form.</p>";
+
+?>
