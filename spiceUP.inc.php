f46dc9ff179ce748ab596125a2c2c8d36e2c5cc1
NationalCoder <webmaster@sociablecooking.com>
2014-06-28 00:16:19 -0500

    SpiceUP form loads Alright, so I found the bastard and it turns out that I had forgotten to put an opening bracket after the else statment in the spiceUP include file. Now I need to figure out why it has the word image in the upper right corner of the dashboard. As well, I need to go through and ensure all my code refrences the right session cookie already know that the updaterecipe include file needs to be updated. Wonder what else I'll find. Tomorrow I'll focus on the script that will insert the spiced recipe into the spiced table.

--- spiceUP.inc.php
+++ spiceUP.inc.php
@@ -11,7 +11,7 @@ $shortdesc = $row['shortdesc'];
 $ingredients = $row['ingredients'];
 $directions = $row['directions'];
 
-if(!isset($_SESSION(['valid_recipe_user']) && $poster == $_SESSION['valid_recipe_user']) {
+if(!isset($_SESSION['recipeuser']) && $poster == $_SESSION['recipeuser']) {
 	echo "<h1>Sorry, your not logged in</h1>\n";
 	echo "<p>Sorry, but you do not have permission to spice-up recipes. For you ";
 	echo "to spice up recieps, you must first register. If you are new here please ";
@@ -20,13 +20,13 @@ if(!isset($_SESSION(['valid_recipe_user']) && $poster == $_SESSION['valid_recipe
 	echo "<a href=\"index.php?card=register\">Registration This Way</a></p>\n";
 	echo "<p>If you are a returning user please use the following link to login.</p>\n";
 	echo "<a href=\"index.php?card=login\">Please login to spice-up recipes</a>\n";
-} else
-	$userid = $_SESSION['valid_recipe_user'];
+} else {
+	$userid = $_SESSION['recipeuser'];
 	echo "<form enctype=\"multipart/form-data\" action=\"index.php\" method=\"post\">\n";
 	echo "<table width=\"80%\" border=\"0\" cellspacing=\"5\" cellpadding=\"0\" align=\"left\">";
 	echo "<tr><td align=\"center\" colspan=\"2\"><h1>Time to Spice Things Up!</h1></td></tr>\n";
 	echo "<tr><td align=\"left\" colspan=\"2\"><recipeTIT>$title</recipeTIT></td></tr>\n";
-	echo "<tr><td align=\"left\" colspan=\"2\"><font size=\"1\" color\"#ff9966\">Created By <em>Chef $poster</em></font></td></tr>\n";
+	echo "<tr><td align=\"left\" colspan=\"2\"><font size=\"1\" color\"#ff9966\">Created By <em>Chef $poster</em> Spiced By <em>Chef $userid</em></font></td></tr>\n";
 
 	echo "<tr><td<h3>Image</h3></td><td><img src=\"showimage.php?id=$recipeid\" width=\"80\" height=\"60\"></td></tr>\n";
 	echo "<tr><td><h3>Update Image</h3></td><td><input type=\"file\" name=\"image\"></td></tr>\n";
