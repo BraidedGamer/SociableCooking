64429e853ea63880b3ea330c7654a1a3e4e2c808
NationalCoder <webmaster@sociablecooking.com>
2014-07-21 22:48:03 -0500

    working on catalog displayb I'm working hard to get the catalog display scripts finished. Once one is finished the rest should fall in line rather quickly. Till then please be patient with me. I'm only one man at present and can only handle so much each day. Just gotta figure out why the recipeid is making this difficult and add a loop, probably foreach loop, to add a recipe from the spiced table after each regular post and finished. Then I might work on figuring out a way to pull the image from the previous table for the spiced form. But I will definately start work on a forgotten password function and then I'll turn the catalog files into separate classes in one file.

--- allrecipes.inc.php
+++ allrecipes.inc.php
@@ -28,15 +28,15 @@ if($row[0] == 0)
 		$recipesperpage = 4;
 		$offset = ($thispage - 1) * $recipesperpage;
 		$totpages = ceil($totrecipes / $recipesperpage);
-		$query = "SELECT * FROM recipes ORDER BY recipeid DESC LIMIT
+		$query = "SELECT recipes.*, spiced.* FROM recipes, spiced ORDER BY recipeid DESC LIMIT
 				$offset, $recipesperpage";
 		$result = mysql_query($query) or die('Could not retrieve recipes: ' .mysql_error());
 		while($row = mysql_fetch_array($result, MYSQL_ASSOC))
 		{
-			$recipeid = $row['recipeid'];
-			$title    = $row['title'];
-			$poster   = $row['poster'];
-			$shortdesc= $row['shortdesc'];
+			$recipeid = $row['recipes.recipeid'];
+			$title    = $row['recipes.title'];
+			$poster   = $row['recipes.poster'];
+			$shortdesc= $row['recipes.shortdesc'];
 
 			echo "<table width=\"95%\" cellpadding=\"0\" \n";
 			echo "cellspacing=\"5\" border=\"0\" align=\"center\">\n";
