b04440835dc1d314b0e1c2a437fc5ab495960fe7
NationalCoder <webmaster@sociablecooking.com>
2014-10-28 15:09:45 -0500

    New Navigation The new navigation is coming along rather quickly and I'm likeing the look of it. Just need to polish off the menu and make sure all the elements are being thrown right into place.

--- nav.inc.php
+++ nav.inc.php
@@ -9,33 +9,25 @@ echo "<input name=\"card\" type=\"hidden\" value=\"search\">\n";
 echo "</form></td></tr>\n";
 echo "</table>\n";
 
-
-echo "<div id=\"nav\">\n";
-
+echo "<ul id=\"nav\">\n";
 	$userid = $_SESSION['recipeuser'];
-	$query = "SELECT fullname FROM users WHERE userid = '$userid'";
-	$result = mysql_query($query);
-	$row = mysql_fetch_array($result, MYSQL_ASSOC);
-	$fullname = $row['fullname'];
-	
-	echo "<table width=\"100%\" border=\"0\" cellspacing=\"5\" cellpadding=\"0\" align=\"right\">\n";
-
 	$cardset = $_REQUEST['card'];
 	if($cardset != 'showrecipe')
 	{
 		$bookSET = $_REQUEST['card'];
+		echo "<li><strong>Catalog</strong></li>\n";
 		if($bookSET != 'community')
 		{
-			echo "<tr><td><a href=\"index.php?card=community\"><strong>Community Recipes</strong></a></td>\n";
+			echo "<ul><li><a href=\"index.php?card=community\"><strong>Community Recipes</strong></a></li></ul>\n";
 		}else if($bookSET != 'dashboard')
 		{
-			echo "<tr><td><a href=\"index.php?card=myrecipes\"><strong>My Recipes</strong></a></td>\n";
+			echo "<ul><li><a href=\"index.php?card=myrecipes\"><strong>My Recipes</strong></a></li></ul>\n";
 		}
 		/* Show Post if your viewing the catalogs, yours or the communities, if
 		   your viewing a single recipe make it vanish and if your viewing a recipe
 		   other than your own display a spice-up link.
 		*/
-		echo "<td><a href=\"index.php?card=newrecipe\"><strong>POST</strong></a></td>\n";
+		echo "<li><a href=\"index.php?card=newrecipe\"><strong>POST</strong></a></li>\n";
 	} else if($cardset = 'showrecipe') {
 		$recipeid = $_REQUEST['id'];
 		$query = "SELECT poster,spicer FROM recipes WHERE recipeid = $recipeid";
@@ -44,17 +36,15 @@ echo "<div id=\"nav\">\n";
 		$poster = $row['poster'];
 		$spicer = $row['spicer'];
 		if($poster != $userid && $spicer == '') {
-			echo "<td><a href=\"index.php?card=spiceUP&id=$recipeid\"><strong>SPICE</strong></a></td>\n";
-		} else {
-			echo "";
-		}
+			echo "<li><a href=\"index.php?card=spiceUP&id=$recipeid\"><strong>SPICE</strong></a></li>\n";
+		} 
 	}
 	$recipeid = $_REQUEST['id'];
 	// show comment, print and edit nav buttons only when card is set to a value of 'showrecipe'
 	if($cardset == 'showrecipe')
 	{
-		echo "<td><a href=\"index.php?card=newcomment&id=$recipeid\"><strong>Comment</strong></a></td>\n";
-		echo "<td><a href=\"print.php?id=$recipeid\" target=\"_blank\"><strong>Print</strong></a></td>\n";
+		echo "<li><a href=\"index.php?card=newcomment&id=$recipeid\"><strong>Comment</strong></a></li>\n";
+		echo "<li><a href=\"print.php?id=$recipeid\" target=\"_blank\"><strong>Print</strong></a></li>\n";
 		
 		$query = "SELECT poster FROM recipes WHERE recipeid = $recipeid";
 		$result = mysql_query($query) or die('Sorry we could not find the requested recipe.');
@@ -64,9 +54,9 @@ echo "<div id=\"nav\">\n";
 		
 		if(isset($_SESSION['recipeuser']) && $poster == $_SESSION['recipeuser'])
 		{
-			echo "<td><a href=\"index.php?card=updaterecipe&id=$recipeid\"><strong>Edit</strong></a></td>\n";
+			echo "<li><a href=\"index.php?card=updaterecipe&id=$recipeid\"><strong>Edit</strong></a></li>\n";
 		} else if($spicer == $userid) {
-			echo "<td><a href=\"index.php?card=updateSpice&id=$recipeid\"><strong>Edit</strong></a></td>\n";
+			echo "<li><a href=\"index.php?card=updateSpice&id=$recipeid\"><strong>Edit</strong></a></li>\n";
 		} else {
 			echo "\n";
 		}
@@ -76,11 +66,10 @@ echo "<div id=\"nav\">\n";
 		echo "\n";
 	}
 	
-	echo "<td><a href=\"index.php?card=myaccount&userid=$userid\">\n";
-	echo "<strong>$userid</strong></a></td>\n";
-	
-	echo "<td><a href=\"index.php?card=logout\">(Logout)</a></td>";
+	echo "<li><strong>$userid</strong></a></li>\n";
+	echo "<ul><li><a href=\"index.php?card=myaccount&userid=$userid\">\n";
+	echo "<strong>Settings</strong></a></li>\n";
+	echo "<li><a href=\"index.php?card=logout\">(Logout)</a></li></ul>";
 
-
-echo "</tr></table></div>\n";
+echo "</ul>\n";
 ?>
