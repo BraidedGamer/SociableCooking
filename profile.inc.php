ac07e0502e7e892c1498a936b99e418d99f2fe03
NationalCoder <webmaster@sociablecooking.com>
2014-12-05 10:56:18 -0600

    More work on the profile display area I've added a div identified as infoDisplay used after the header option in the profile sidebar. Still trying to decide how to add gender, birthdate and relationship status into the database!

--- profile.inc.php
+++ profile.inc.php
@@ -16,24 +16,44 @@ $firstName = $row['firstName'];
 $lastName  = $row['lastName'];
 /* This is the code for the about section of the profile. */
 echo "<h4>About</h4>\n";
-	echo "<table width=\"60%\" cellpadding=\".5px\" cellspacing=\"1px\" \n";
-	echo "border=\"0px\" align=\"left\">\n";
+	echo "<div id=\"infoDisplay\">\n";
+		echo "<table width=\"70%\" cellpadding=\".5px\" cellspacing=\"1px\" \n";
+		echo "border=\"0px\" align=\"left\">\n";
 			echo "<tr>\n";
 				echo "<td align=\"left\">Real Name:</td>\n";
 				echo "<td align=\"left\"><i>$lastName, $firstName</i></td>\n";
 			echo "</tr>\n";
 			echo "<tr>\n";
+				echo "<td align=\"left\">Recipe Count:</td>\n";
+				$query = "SELECT count(recipeid) FROM recipes WHERE poster = '$userid' OR spicer = '$userid'";
+				$result = mysql_query($query);
+				$row = mysql_fetch_array($result);
+				if($row[0] == 0) {
+					echo "<td align=\"left\"><i>You haven't shared any recipes with us!</i></td>\n";
+				} else {
+					$totrecipes = $row[0];
+					echo "<td align=\"left\"><i>You have $totrecipes recipes!</i></td>\n";
+				}
+			echo "</tr>\n";
+			echo "<tr>\n";
 				echo "<td align=\"left\">Gender:</td>\n";
-				echo "<td align=\"left\"><i></i></td>\n";
+				echo "<td align=\"left\"><i>male or female</i></td>\n";
 			echo "</tr>\n";
 			echo "<tr>\n";
 				echo "<td align=\"left\">Birthday:</td>\n";
-				echo "<td align=\"left\"><i></i></td>\n";
+				echo "<td align=\"left\"><i>birthdate</i></td>\n";
 			echo "</tr>\n";
 			echo "<tr>\n";
 				echo "<td align=\"left\">Relationship:</td>\n";
-				echo "<td align=\"left\"><i></i></td>\n";
+				echo "<td align=\"left\"><i>relationship status</i></td>\n";
 			echo "</tr>\n";
-	echo "</table>\n";
+		echo "</table>\n";
+	echo "</div>\n";
+/* This is the end of the about section and the start of the bio section */
+echo "<h4>Biography</h4>\n";
+	echo "<div id=\"infoDisplay\">\n";
+		echo "<p>This is a Biography section to be filled up later!</p>\n";
+	echo "</div>\n";
+/* This is the end of the bio section */
 ?>
 
