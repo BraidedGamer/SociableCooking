c28903d6c21b1abd76aa7d4df5e075cd3a3468a3
Theodore Philip Rees <theodore.rees@gmail.com>
2018-08-03 16:14:05 -0500

    needed to remove a folder that I don't where it came from

--- _/footer.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "<table width=\"350\" border=\"0\" cellspacing=\"5\" cellpadding=\"0\">\n";
-echo "<tr><td><font size=\"1\" color=\"#cc9966\">&copy; " . date("Y") . " Ted's Unique Area Graphics &middot; All Rights Reserved</font></td>\n";
-echo "<td><a href=\"index.php?card=about\">About</a></td>\n";
-echo "<td><a href=\"index.php?card=contact\">Contact</a></td></tr>\n";
-echo "</table>\n";
-
-?>
